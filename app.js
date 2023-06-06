8bf2027788900b18fd4c098f3c1b325062f00f62
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-06 12:25:55 +0000

    day 4

--- app.js
+++ app.js
@@ -2,41 +2,31 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
+var items = [ 'Buy Food' , 'Cook Food', 'Eat Food']
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
 
     var today = new Date()
-    var currentDay = today.getDay()
-    var day = ""
+    var options ={
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = today.toLocaleDateString("en-US", options)
+    
+    res.render("lists", { kindofDay: day , kindofItem: items})
+})
 
-   switch (currentDay) {
-     case 0:
-        day = "Sunday"
-        break;
-       case 1:
-           day = "Monday"
-           break;
-       case 2:
-           day = "Tuesday"
-           break;
-       case 3:
-           day = "Wednesday"
-           break;
-       case 4:
-           day = "Thursday"
-           break;
-       case 5:
-           day = "Friday"
-           break;
-       case 6:
-           day = "Saturday"
-           break;
-    default:
-        console.log("Error: current day is equal to: " + currentDay)
-   }
-    res.render("lists", {kindofDay: day})
+app.post("/", function(req, res){
+   var item = req.body.newItem
+   items.push(item)
+
+   res.redirect("/")
 })
 
 app.listen(process.env.PORT || 3000, function(){
