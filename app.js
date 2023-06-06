bddee20c729ae9eecca573f6c1b745e1204b9ab5
Muttyfaw <106316623+Muttyfaw@users.noreply.github.com>
2023-06-06 16:19:48 +0000

    Almost there

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express")
 const bodyParser = require("body-parser")
 
 const app = express()
-var items = [ 'Buy Food' , 'Cook Food', 'Eat Food']
+let items = [ 'Buy Food' , 'Cook Food', 'Eat Food']
+let workItems = []
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
@@ -11,23 +12,36 @@ app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
 
-    var today = new Date()
-    var options ={
+    let today = new Date()
+    let options ={
         weekday: "long",
         day: "numeric",
         month: "long"
     }
-    var day = today.toLocaleDateString("en-US", options)
+    let day = today.toLocaleDateString("en-US", options)
     
-    res.render("lists", { kindofDay: day , kindofItem: items})
+    res.render("lists", { listTitle: day , kindofItem: items})
 })
 
+
 app.post("/", function(req, res){
-   var item = req.body.newItem
+   let item = req.body.newItem
    items.push(item)
 
    res.redirect("/")
 })
+
+app.get("/work", function (req, res) {
+    res.render("lists", { listTitle: "Work List", kindofItem: workItems })
+})
+
+app.post("/work", function (req, res) {
+    let item = req.body.newItem
+    workItems.push(item)
+
+    res.redirect('/work')
+})
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000")
